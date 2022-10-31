718d1600e72527c56ccf16b479b82c38250c6514
rana <trana51997@gmail.com>
2022-10-31 13:30:39 -0500

    Added React DOM portal

--- src/components/UI/InvalidUser/InvalidUsernameAge.js
+++ src/components/UI/InvalidUser/InvalidUsernameAge.js
@@ -1,22 +1,43 @@
 import styles from "./InvalidUsernameAge.module.css";
 import Button from "../Button/Button";
 import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button name="Okay" onClick={props.onConfirm}></Button>
+      </footer>
+    </div>
+  );
+};
 
 const InvalidUsernameAge = (props) => {
   return (
     <Fragment>
-      <div className={styles.backdrop} onClick={props.onConfirm} />
-      <div className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button name="Okay" onClick={props.onConfirm}></Button>
-        </footer>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </Fragment>
   );
 };
